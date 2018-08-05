765b4adf600aa4e1c9ae32a102c1175908981097
trndo <vetal1199@gmail.com>
2018-08-05 20:31:06 +0300

    Add functional tests for UserType and ArtistType

---
+++ tests/Forms/ArtistTypeTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vel-vet
+ * Date: 05.08.18
+ * Time: 20:17
+ */
+
+namespace App\Tests\Forms;
+
+use App\Entity\Artists;
+use App\Forms\ArtistType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class ArtistTypeTest extends TypeTestCase
+{
+
+    public function testSubmitData()
+    {
+        $formData =[
+            'artistName'=>'Imagine Dragons',
+            'year'=>'Imagine Dragons',
+        ];
+
+        $objectToCompare = new Artists();
+
+        $form = $this->factory->create(ArtistType::class, $objectToCompare);
+
+        $object = new Artists();
+
+        $form->submit($formData);
+
+        self::assertTrue($form->isSynchronized());
+
+        self::assertEquals($object,$objectToCompare);
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            self::assertArrayHasKey($key, $children);
+        }
+    }
+
+}
---
+++ tests/Forms/UserTypeTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vel-vet
+ * Date: 05.08.18
+ * Time: 20:28
+ */
+
+namespace App\Tests\Forms;
+
+use App\Forms\UserType;
+use App\Entity\User;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class UserTypeTest extends TypeTestCase
+{
+    public function testSubmitData()
+    {
+        $formData =[
+            'email'=>'vetal1199@gmail.com',
+            'username'=>'trndo',
+            'plainPassword'=>'1234'
+        ];
+
+        $objectToCompare = new User();
+
+        $form = $this->factory->create(UserType::class, $objectToCompare);
+
+        $object = new User();
+
+        $form->submit($formData);
+
+        self::assertTrue($form->isSynchronized());
+
+        self::assertEquals($object,$objectToCompare);
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            self::assertArrayHasKey($key, $children);
+        }
+    }
+}
